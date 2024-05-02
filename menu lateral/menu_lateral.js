678f0aba2ec2e73e610e4c174cfbbb40c031e067
keneth <keneth.javier90@gmail.com>
2024-05-02 17:32:17 -0600

    keneth version1

--- menu lateral/menu_lateral.js
+++ menu lateral/menu_lateral.js
@@ -1,1 +1,20 @@
-.
+var liElement = document.querySelector(".a2");
+
+
+liElement.innerHTML = 'cpu';
+
+var liElement = document.querySelector(".a3");
+liElement.innerHTML = 'gpu'
+
+var liElement = document.querySelector(".a4");
+liElement.innerHTML = 'md'
+
+var liElement = document.querySelector(".a5");
+liElement.innerHTML = 'RAM'
+
+var liElement = document.querySelector(".a6");
+liElement.innerHTML = 'HDD'
+
+
+
+
